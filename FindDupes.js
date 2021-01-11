0bea5eb4f1617db063e365bc7e30aa26f4b104f2
Rudy Becker <52896529+RudyBecker@users.noreply.github.com>
2021-01-11 15:16:52 -0800

    Create FindDupes.js

---
+++ FindDupes.js
@@ -0,0 +1,10 @@
+let arrayOne = [2, 4, 6, 8, 10];
+let arrayTwo = [4, 6, 7, 8, true, "string"];
+
+function findDupes(arrayOne, arrayTwo) {
+  let filteredArrayOne = new Set(arrayOne);
+  let finalArray = arrayTwo.filter((n) => filteredArrayOne.has(n));
+  return finalArray;
+}
+
+console.log(findDupes(arrayOne, arrayTwo));
